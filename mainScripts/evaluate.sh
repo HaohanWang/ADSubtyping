3716c8bcd1a95c74d03adfcf992ddaa43dc25ced
Haohan Wang <haohanw@andrew.cmu.edu>
2021-08-31 10:24:27 -0400

    code update

--- mainScripts/evaluate.sh
+++ mainScripts/evaluate.sh
@@ -7,15 +7,15 @@ done
 
 for i in {15..50..5}
 do
-     python model.py -f weights_minMaxPool -m 1 -u 1 -a 1 -b 8 -c $i
+     python model.py -f weights_minMaxPool -n 1 -u 1 -a 1 -b 8 -c $i
 done
 
 for i in {15..50..5}
 do
-     python model.py -f weights_minMaxPool -p 0.01 -u 1 -a 1 -b 8 -c $i
+     python model.py -f weights_pgd -p 0.01 -u 1 -a 1 -b 8 -c $i
 done
 
 for i in {15..50..5}
 do
-     python model.py -f weights_minMaxPool -p 0.05 -u 1 -a 1 -b 8 -c $i
+     python model.py -f weights_pgd -p 0.05 -u 1 -a 1 -b 8 -c $i
 done
