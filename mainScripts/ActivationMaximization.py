9ecdc97afe905265be8c250c7f7e6f46bfcde06c
gn03249822 <dah0417912@gmail.com>
2021-11-05 15:09:48 -0400

    parametrizing feature extraction done

--- mainScripts/ActivationMaximization.py
+++ mainScripts/ActivationMaximization.py
@@ -2,67 +2,42 @@ import numpy as np
 import tensorflow as tf
 
 
+DEFAULT_SAVE_PATH = "./activation_visualization/"
 """
 Class to compute and visualize activation maximizations for different CNN filter layers 
 """
 
-
 class ActivationMaximizer(object):
-    def __init__(self, model, iters=30, lr=10.0):
+    # TODO hyperparameters tuning
+    def __init__(self, model, intermediate_ly_idx=2, iters=15, lr=10):
         self.model = model
+        self.layer_idx = intermediate_ly_idx
         self.iters = iters
         self.learning_rate = lr
 
-    def compute_loss(self, input_image, filter_index):
-        activation = self.model(input_image, training=False)
-        # We avoid border artifacts by only involving non-border pixels in the loss.
-        # TODO is this necessary
-        filter_activation = activation[:, 2:-2, 2:-2, 2:-2, filter_index]
-        return tf.reduce_mean(filter_activation)
-
-    def initialize_image(self):
-        # TODO verify dimensions of 3D images and the expected range of input values
-        img = tf.random.uniform(shape=(169, 208, 179))
-        return img
+    def compute_loss(self, images):
+        activation = self.model.extract_embedding(images, self.layer_idx)
+        # cropping is done scientifically during MRI data postprocessing
+        return tf.reduce_mean(activation)
 
     @tf.function
-    def gradient_ascent_step(self, img, filter_index, learning_rate):
+    def gradient_ascent_step(self, images, learning_rate):
+        images = tf.convert_to_tensor(images)
         with tf.GradientTape() as tape:
-            tape.watch(img)
-            loss = self.compute_loss(img, filter_index)
+            tape.watch(images)
+            loss = self.compute_loss(images)
         # Compute gradients.
-        grads = tape.gradient(loss, img)
+        grads = tape.gradient(loss, images).numpy()
         # Normalize gradients.
         grads = tf.math.l2_normalize(grads)
-        img += learning_rate * grads
-        return loss, img
+        images += learning_rate * grads
+        return loss, images
 
-    def visualize_activation(self, filter_index):
-        # We run gradient ascent for 30 steps
-        img = self.initialize_image()
-        loss = None
+    def visualize_activation(self, dataset, total_batch_steps, save_dir=DEFAULT_SAVE_PATH):
+        for i in range(total_batch_steps):
+            images, labels, subject_ids, session_ids = dataset[i]
+            for iteration in range(self.iters):
+                loss, computed_images = self.gradient_ascent_step(images, self.learning_rate)
 
-        for iteration in range(self.iters):
-            loss, img = self.gradient_ascent_step(img, filter_index, self.learning_rate)
-
-        # Decode the resulting input image
-        img = self.deprocess_image(img[0].numpy())
-        return loss, img
-
-    def deprocess_image(self, img):
-        # Normalize array: center on 0., ensure variance is 0.15
-        img -= img.mean()
-        img /= img.std() + 1e-5
-        img *= 0.15
-
-        # Center crop
-        img = img[25:-25, 25:-25, 25:-25, :]
-
-        # Clip to [0, 1]
-        img += 0.5
-        img = np.clip(img, 0, 1)
-
-        # Convert to RGB array
-        # img *= 255
-        # img = np.clip(img, 0, 255).astype("uint8")
-        return img
+            for img, subject_id, session_id in zip(computed_images, subject_ids, session_ids):
+                np.save("{}/{}_{}_activation_layer_{}.npy".format(save_dir, subject_id, session_id, self.layer_idx), img)
