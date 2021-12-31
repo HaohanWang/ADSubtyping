a4242578e1e94d082ff72aa3f1e678b828565ad1
gn03249822 <dah0417912@gmail.com>
2021-12-31 14:34:49 -0800

    policy path

--- visualization/saliency.py
+++ visualization/saliency.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
 
     # model = model_checkpoint("results/policy_eps5e-3_lr1e-5_weight_dropblock/checkpoint/epoch_0010.pth")
 
-    config = config_utils.load("/home/ec2-user/alzstudy/code/mainScripts_torch/policies/policy_2.yml")
+    config = config_utils.load("/home/ec2-user/alzstudy/code/AlzheimerDiseaseUnderstanding/mainScripts_torch/policies/policy_2.yml")
     model = model_checkpoint("/home/ec2-user/alzstudy/checkpoints/policy2_1e-5_dr_0.5_eps_5e-3_seed_0/checkpoint/epoch_0010.pth")
     model.cuda()
     dataloaders = {split:get_dataloader(split, batch_size=batch_size)
