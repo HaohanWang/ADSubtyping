18c7c322af5bcd753755778882ee6eb110fa5b24
gn03249822 <dah0417912@gmail.com>
2021-12-31 14:42:19 -0800

    policy update

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -18,6 +18,7 @@ import math
 
 import dataManagement.dataset as ds
 import mainScripts_torch.model as torch_model
+import utility.config_utils as config_utils
 
 
 class MinMaxNormalization(object):
@@ -25,7 +26,7 @@ class MinMaxNormalization(object):
     def __call__(self, image):
         return (image - image.min()) / (image.max() - image.min())
 
-def model_checkpoint(checkpoint_path, feature=None):
+def model_checkpoint(checkpoint_path, feature=None, config=None):
     checkpoint = torch.load(checkpoint_path)
     checkpoint_dict = {}
     for k, v in checkpoint['state_dict'].items():
@@ -41,10 +42,11 @@ def model_checkpoint(checkpoint_path, feature=None):
                 checkpoint_dict[k] = v
             else:
                 checkpoint_dict['feature_extractor.' + k] = v
-    config = edict()
-    config.model = edict()
-    config.model.name = 'Conv5_FC3'
-    config.model.params = None
+    if config is None:
+        config = edict()
+        config.model = edict()
+        config.model.name = 'Conv5_FC3'
+        config.model.params = None
     model = torch_model.get_model(config)
     model.load_state_dict(checkpoint_dict)
     
@@ -131,6 +133,7 @@ if __name__ == "__main__":
     makedirs(save_dir, exist_ok=True)
 
     # model = model_checkpoint("results/policy_eps5e-3_lr1e-5_weight/checkpoint/epoch_0017.pth")
+    config = config_utils.load("/home/ec2-user/alzstudy/code/AlzheimerDiseaseUnderstanding/mainScripts_torch/policies/policy_2.yml")
     model = model_checkpoint("/home/ec2-user/alzstudy/checkpoints/policy2_1e-5_dr_0.5_eps_5e-3_seed_0/checkpoint/epoch_0010.pth")
     model.cuda()
     attack = torchattacks.PGD(model, eps = 0.05, alpha = 0.00125, iters=10, random_start=True)
