f549890b876a10f98dfaaaf679c011a1e265f10e
gn03249822 <dah0417912@gmail.com>
2022-06-18 19:33:55 -0700

    v2 script

--- mainScripts/MCIFinetuneDataCleaning.py
+++ mainScripts/MCIFinetuneDataCleaning.py
@@ -6,29 +6,25 @@ from collections import defaultdict
 READ_DIR = '/home/ec2-user/mnt/home/ec2-user/alzstudy/AlzheimerData/'
 
 
-
 # return the list of subjects that satisfy our criteria for finetuning
-def find_mci_subjects(img_dir=READ_DIR + 'ADNI_CAPS'):
+def find_mci_subjects(img_dir=READ_DIR + 'ADNI_CAPS', idx_fold=0):
     subjects_to_labels = defaultdict(list) # subject: [label]
 
 
-    for idx_fold in [0, 1, 2, 3, 4]:
-        csv_path = join(img_dir, f'split.pretrained.{idx_fold}.csv')
-        text = [line.strip() for line in open(csv_path)]
+    csv_path = join(img_dir, f'split.pretrained.{idx_fold}.csv')
+    text = [line.strip() for line in open(csv_path)]
 
+    for line in text[1:]:
+        items = line.split(',')
 
-        for line in text[1:]:
-            items = line.split(',')
-
-            split = items[-1]
-            csv_label = items[-2]
-            subject = items[0]
-            session = items[1]
-
-            subjects_to_labels[subject].append(split, session, csv_label, 'split: {} session:{} label: {} ')
-
+        split = items[-1]
+        csv_label = items[-2]
+        subject = items[0]
+        session = items[1]
+        subjects_to_labels[subject].append(f'split: {split} session:{session} label: {csv_label}')
 
     print(subjects_to_labels)
+
 
 find_mci_subjects()
 
